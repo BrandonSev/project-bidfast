fcc153aad8365888279dd133d3dc0a1c8a87a6fd
BrandonSev <seveste.brandon@gmail.com>
2021-10-31 18:50:46 +0100

    feat: Footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {Route, Switch} from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Switch>
         <Route path='/' exact component={Home}/>
       </Switch>
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <span className="footer__border"></span>
+      <div className="footer__body">
+        <ul className="footer__menu">
+          <li>Accueil</li>
+          <li>Acheter</li>
+          <li>Vendre</li>
+          <li>Rechercher</li>
+        </ul>
+        <p className="footer__copyright">Copyright © 2021 • Brandon Seveste</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
