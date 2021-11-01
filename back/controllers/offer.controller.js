b56191a33904b7321b7e4d1b94b6ff60b0a75e9f
BrandonSev <seveste.brandon@gmail.com>
2021-11-01 20:34:25 +0100

    feat(back): route pour recuperer toutes les offres

--- back/controllers/offer.controller.js
+++ back/controllers/offer.controller.js
@@ -1,5 +1,12 @@
 const db = require('../database')
 
+module.exports.getAll = async (req, res) => {
+  db.query('SELECT * FROM offers', function (err, result) {
+    if (err) return res.status(400).send(err)
+    return res.status(200).send(result)
+  })
+}
+
 module.exports.offerCreate = async (req, res) => {
   const {name, content, image, expireAt, startPrice} = req.body
   const userId = res.locals.user.id
--- back/routes/offer.routes.js
+++ back/routes/offer.routes.js
@@ -2,6 +2,9 @@ const offerController = require("../controllers/offer.controller");
 const {isAuthenticated} = require("../middleware/auth.middleware");
 const router = require("express").Router();
 
+// GET
+router.get('/', offerController.getAll)
+//POST
 router.post('/create', isAuthenticated, offerController.offerCreate)
 
 module.exports = router;
