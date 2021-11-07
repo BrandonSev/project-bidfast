64112faa65ac0c065231e23804b54e9b73f2d16e
BrandonSev <seveste.brandon@gmail.com>
2021-11-07 20:24:25 +0100

    feat(page): création page dépot annonce page profile

---
+++ src/components/NewBid/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const NewBid = () => {
+  return (
+    <div className="container">
+      <h2>Déposer une annonce</h2>
+      <form action="#" className="new__bid">
+        <div className="grid">
+          <div className="grid__group">
+            <label htmlFor="titre">Titre de l'annonce:</label>
+            <input type="text" id="titre" />
+          </div>
+          <div className="grid__group">
+            <label htmlFor="titre">Catégorie:</label>
+            <input type="text" id="titre" />
+          </div>
+          <div className="grid__group">
+            <label htmlFor="titre">Date d'expiration:</label>
+            <input type="text" id="titre" />
+          </div>
+          <div className="grid__group">
+            <label htmlFor="titre">Image:</label>
+            <input type="file" id="titre" />
+          </div>
+          <div className="grid__group">
+            <label htmlFor="titre">Description:</label>
+            <textarea id="titre" />
+          </div>
+        </div>
+        <button className="btn btn-small btn-primary">Envoyer</button>
+      </form>
+    </div>
+  );
+};
+
+export default NewBid;
